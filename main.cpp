4a8832179c1efd58da899b682eea78eb1b860944
Dawid Bogocz <dawibog325@student.polsl.pl>
2020-10-22 15:15:43 +0200

    Adding Matrix to Matrix

--- main.cpp
+++ main.cpp
@@ -2,31 +2,86 @@
 #include <vector>
 #include <array>
 
-struct Matrix2D{
+struct Matrix2D {
     std::vector<std::vector<int>> contents;
 
-    void print(){
-        std::cout<<"Contents of the matrix: "<<std::endl;
+    void add( Matrix2D other) {
+        if (other.rows() == rows() && other.columns() == columns()) {
 
-        for(int i=0; i<3; i++){
-            for(int j=0; j<3; j++){
-                std::cout<<contents[i][j]<<" ";
+            for (int i = 0; i < rows(); i++) {
+                for (int j = 0; j < columns(); j++) {
+                    contents[i][j] += other.contents[i][j];
+                }
             }
-            std::cout<<std::endl;
+        }else {
+            std::cout << "Dimensions must match!" << std::endl;
+        }
+    }
+
+    int rows() {
+        return contents.size();
+    }
+
+    int columns() {
+        return contents[0].size();
+    }
+
+    void input_matrix() {
+        int columns, rows;
+
+        std::cout << "Number of rows: " << std::endl;
+        std::cin >> rows;
+
+        std::cout << "Number of columns: " << std::endl;
+        std::cin >> columns;
+
+        for (int i = 0; i < rows; i++) {
+            std::vector<int> row;
+            int tmp;
+            std::cout << "Introduce a row with " << columns << " columns" << std::endl;
+            for (int j = 0; j < columns; j++) {
+                std::cout << "Introduce value: " << std::endl;
+                std::cin >> tmp;
+                row.push_back(tmp);
+            }
+            contents.push_back(row);
+        }
+    }
+
+    void print() {
+        std::cout << "Contents of the matrix: " << std::endl;
+
+
+        int rows = contents.size();
+        int cols = contents[0].size();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                std::cout << contents[i][j] << " ";
+            }
+            std::cout << std::endl;
         }
     }
 };
 
 int main()
 {
-    Matrix2D a;
-    Matrix2D b;
+    Matrix2D a, b;
 
-    a.contents = {{1,2,3},{4,5,6},{7,8,9}};
-    b.contents = {{9,8,7},{6,5,4},{3,2,1}};
+    std::cout<<"Introduce data for matrix a: "<<std::endl;
+    a.input_matrix();
+    std::cout<<"Introduce data for matrix b: "<<std::endl;
+    b.input_matrix();
 
+    std::cout<<"Print matrix a: "<<std::endl;
     a.print();
+    std::cout<<"Print matrix b: "<<std::endl;
     b.print();
+
+    std::cout<<"Adding b to a: "<<std::endl;
+    a.add(b);
+    a.print();
 
     return 0;
 }
+
