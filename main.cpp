e144e93b30e0c2c257b9e5c98d503c6f10430231
Dawid Bogocz <dawibog325@student.polsl.pl>
2020-10-22 12:40:34 +0200

    Function to initialize Points by reference

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <array>
+#include <cstdlib>
 
 //Define a struct
 struct Point2D{
@@ -7,17 +7,34 @@ struct Point2D{
     double y;
 };
 
+Point2D create_point(){
+    Point2D tmp;
+    tmp.x=rand()%10;
+    tmp.y=rand()%10;
+
+    return tmp;
+}
+
+void initialize_in_place(Point2D& point){
+    point.x=rand()%10;
+    point.y=rand()%10;
+}
+
 int main()
 {
     //Declare a variable with our struct
-    Point2D mypoint;
-
-    mypoint.x=3.3;
-    mypoint.y=5.5;
+    Point2D mypoint=create_point();
 
     //Print its contents
     std::cout<<"Value of X: "<<mypoint.x<<std::endl;
     std::cout<<"Value of Y: "<<mypoint.y<<std::endl;
+
+    Point2D empty_point;
+    initialize_in_place(empty_point);
+
+    std::cout<<"Value of X: "<<empty_point.x<<std::endl;
+    std::cout<<"Value of Y: "<<empty_point.y<<std::endl;
+
 
     return 0;
 }
